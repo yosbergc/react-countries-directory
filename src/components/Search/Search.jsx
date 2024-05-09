5c61e1a32b8e01b7507e9f83a19bb8efe91089f2
Yosber Guerrero <yosbergc@gmail.com>
2024-05-09 13:21:53 -0400

    Added search style

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,10 @@
 import SearchIcon from '../../assets/svg/Search.svg'
+import './Search.css'
 function Search () {
     return (
         <section className="search-container">
             <img src={SearchIcon} alt="Search Icon" />
-            <input type="text" />
+            <input type="text" placeholder='Search by Name, Region, Subregion'/>
         </section>
     )
 }
