88df28d8f81bd1ef5c1025709a79d827cd439f55
Yosber Guerrero <yosbergc@gmail.com>
2024-05-12 11:14:43 -0400

    Added React Router for pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { CountryPage } from './routes/CountryPage/CountryPage.jsx'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Router>
+        <Routes>
+            <Route path='/' Component={App} />
+            <Route path='/country/:country' Component={CountryPage} />
+        </Routes>
+    </Router>
 )
---
+++ src/routes/CountryPage/CountryPage.jsx
@@ -0,0 +1,6 @@
+function CountryPage() {
+    return (<section>
+        <h2>HOLA</h2>
+    </section>)
+}
+export {CountryPage}
